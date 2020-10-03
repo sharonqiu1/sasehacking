7b747ee3725eb0e2193c1dc3143b9c3681f87618
Sharon Qiu <sharonqiu1@gmail.com>
2020-10-03 17:55:41 -0400

    Added big title

--- covid_track/App.js
+++ covid_track/App.js
@@ -1,7 +1,6 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomePage from './pages/HomePage.js';
@@ -16,6 +15,16 @@ export default function App() {
         <Stack.Screen
           name="HomePage"
           component={HomePage}
+          options={{
+            title: 'HomePage',
+            headerStyle: {
+              backgroundColor: '#ADD8E6',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}
         />
         <Stack.Screen
           name="Symptom Selection"
@@ -25,12 +34,3 @@ export default function App() {
     </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- covid_track/pages/HomePage.js
+++ covid_track/pages/HomePage.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
-import { Text, Button } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
 
 class HomePage extends Component {
   render() {
     return (
-      <Button
-      title="Get Started"
-      onPress={() =>
-        this.props.navigation.navigate('Symptom Selection')
-      }
-    />
+      <View>
+        <Text style={styles.covidTitle}>
+          Covid Tracker
+        </Text>
+        <Button
+        title="Get Started"
+        onPress={() =>
+          this.props.navigation.navigate('Symptom Selection')
+        }
+      />
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  covidTitle: {
+    marginTop: 40,
+    fontSize: 100,
+    textAlign: 'center',
+    textAlignVertical: 'center',
+  },
+});
 
 export default HomePage;
